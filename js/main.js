b5d19aa09a55a922d4f2334b1d0cc09c4a645955
yasell <strogofcdm@gmail.com>
2016-03-31 18:10:27 +0300

    Retina graphics final

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-$(document).ready(function($) {
-  $("body").on("click", ".page-header__trigger", function() {
-    if ($("body").hasClass("menu-open")) {
-      $("body").removeClass("menu-open");
-      $("body").addClass("menu-closed");
-    } else {
-      $("body").removeClass("menu-closed");
-      $("body").addClass("menu-open");
-    }
-  });
-});
+var menuBtn = document.getElementById('menu-icon');
+var menuContainer = document.getElementById('body');
+
+var menuContClosed = "menu-closed";
+var menuContOpened = "menu-open";
+
+menuBtn.onclick = function() {
+  if (menuContainer.className == menuContClosed) {
+    menuContainer.className = menuContOpened;
+  } else if (menuContainer.className == menuContOpened) {
+    menuContainer.className = menuContClosed;
+  }
+}
